bfb9226e66aaf1fd494f6f6d528b5eab1ac58437
apolinarii <borodesku@gmail.com>
2022-05-04 02:16:47 +0300

    two commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(1);
+        int [] weight = new int [3];
+        weight[0] = 50;
+        weight[1] = 60;
+        weight[2] = 70;
+        int december = weight[0];
+        int january = weight[1];
+        int march = weight[2];
+        System.out.println(december);
+        System.out.println(january);
+        System.out.println(march);
+
+        double [] weightTwo = {1.57, 7.654, 9.986 };
+        int[] weightFree = new int [10];
+
+        int [] a = new int [3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        System.out.println(a[0] + ",");
+        System.out.println(a[1] + ",");
+        System.out.println(a[2] + "   ");
+        double[] b = new double [3];
+        b[0] = 1.57;
+        b[1] = 7.654;
+        b[2] = 9.986;
+        System.out.println(b[0] + ",");
+        System.out.println(b[1] + ",");
+        System.out.println(b[2] + "   ");
+        int[] mass = {2, 5, 77};
+        for (int u = 0; u < mass.length; u++) {
+            if (u == mass.length - 1)
+                System.out.println(mass[u]);
+            else
+                System.out.println(mass[u] + ",");
+        }
+
+
+
     }
 }
