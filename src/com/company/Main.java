8dd6ac46072f780fa8093c0721d5eb87cccf933f
apolinarii <borodesku@gmail.com>
2022-05-15 16:04:18 +0300

    four commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -40,11 +40,8 @@ public class Main {
         for (int y = 0; y < numbers.length; y++) {
             if (numbers[y] % 2 != 0) {
                 numbers[y] = numbers[y] + 1;
-            } else if (numbers[y] % 2 == 0) {
+                System.out.print(numbers[y] + ", ");
             }
-            System.out.print(numbers[y] + ", ");
-
-
 
     }
 }
